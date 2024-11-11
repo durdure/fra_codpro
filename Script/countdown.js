a00461d0e25625ecb2057314b76a04d83ccb1572
Duresa Eshetu <duresaeshetu2001@gmail.com>
2024-11-11 17:41:56 +0300

    fra

--- Script/countdown.js
+++ Script/countdown.js
@@ -1,34 +1,41 @@
 const countdownContainer = document.getElementById("countdown-container");
 
-const targetDate = new Date("2023-12-31T23:59:59").getTime();
+const countdownDuration = 0.1 * 60 * 1000; 
+const targetDate = new Date().getTime() + countdownDuration;
 
 function startCountdown() {
   const currentDate = new Date().getTime();
-
   const timeRemaining = targetDate - currentDate;
 
-  const daysElement = document.getElementById("day");
-  const hoursElement = document.getElementById("hour");
   const minutesElement = document.getElementById("minute");
   const secondsElement = document.getElementById("second");
 
-  let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  let hours = Math.floor(
-    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-hours = String(hours).padStart(2, "0");
-  minutes = String(minutes).padStart(2, "0");
-  seconds = String(seconds).padStart(2, "0");
+  if (timeRemaining <= 0) {
+    minutesElement.textContent = "00";
+    secondsElement.textContent = "00";
+    clearInterval(intervalId); 
+    alert("Count Ended")
+    return;
+  }
 
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  hoursElement.textContent = hours;
-  minutesElement.textContent = minutes;
-  secondsElement.textContent = seconds;
+  minutesElement.textContent = String(minutes).padStart(2, "0");
+  secondsElement.textContent = String(seconds).padStart(2, "0");
 
 }
 
 
-setInterval(startCountdown, 1000);
+function showAlert() {
+  const alertBox = document.getElementById("custom-alert");
+  alertBox.style.display = "flex";
+}
 
+function closeAlert() {
+  const alertBox = document.getElementById("custom-alert");
+  alertBox.style.display = "none";
+}
+
+
+const intervalId = setInterval(startCountdown, 1000);
